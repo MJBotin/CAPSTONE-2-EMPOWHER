862a475a03612b8a511b67dea4c38384e455c9cb
mckl18 <antoniomickel732@gmail.com>
2025-11-05 13:26:00 +0800

    feat: adding an announcement model, migration, factory, and seeders and some changes for the user table nd registration status

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,4 +39,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function announcements()
+    {
+    return $this->hasMany(\App\Models\Announcement::class, 'created_by');
+    }
+
 }
