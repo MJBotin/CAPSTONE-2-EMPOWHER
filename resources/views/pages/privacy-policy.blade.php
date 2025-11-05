9f0c9d70ce4acae1ff096ea1f563321c81df151f
rncdrc <roncedric05@gmail.com>
2025-11-05 14:46:37 +0800

    feat: created register form

---
+++ resources/views/pages/privacy-policy.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-24">
+        
+        <div class="text-center">
+            <h1 class="text-3xl font-bold tracking-tight text-[#000000] dark:text-white sm:text-4xl">
+                Privacy Policy
+            </h1>
+        </div>
+
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/privacy-policy', function () {
+    return view('pages.privacy-policy');
+})->name('privacy.policy');
