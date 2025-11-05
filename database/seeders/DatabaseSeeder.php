6c65c7021152a9e44396e713bf89c16c1f5c35ea
mckl18 <antoniomickel732@gmail.com>
2025-11-05 14:05:01 +0800

    feat: adding an auditlogs file model, migration, factory, and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             DocumentRequestSeeder::class,
             AnnouncementSeeder::class,
             BarangayOfficialSeeder::class,
+            AuditLogSeeder::class,
+
         ]);
     }
 
