c77f57dd4068200a427cdfbb73451410bc28f465
mckl18 <antoniomickel732@gmail.com>
2025-11-06 10:03:26 +0800

    fix: updated the route web

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,6 @@ Route::middleware('auth')->group(function () {
     Route::put('/users/{id}/archive', [UserController::class, 'archive'])->name('users.archive');
 });
 
-Route::get('/register', function () {
-    return view('auth.register');
-});
-
 Route::get('/privacy-policy', function () {
     return view('pages.privacy-policy');
 })->name('privacy.policy');
